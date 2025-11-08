3bddb45b61912b73d535ae4e461465941b38e665
Justin Chou <justinjchou@gmail.com>
2025-11-08 10:13:06 -0800

    added kelvin conversion

--- optionfahrtocelsius/main.c
+++ optionfahrtocelsius/main.c
@@ -2,10 +2,11 @@
 
 int main()
 {
-	float fahr, celsius;
+	float fahr, celsius, kelvin;
 	int c;
 	
 	printf("Press 1 for Fahrenheit to Celsisus and 2 for Celsisus to Fahrenheit\n");
+	printf("Press 3 for Fahrenheit to Kelvin\n");
 	c = getchar();
 
 	if(c == '1')
@@ -24,6 +25,15 @@ int main()
 		{
 			fahr = (celsius * 9.0 / 5.0) + 32.0;
 			printf("Fahrenheit: %f\n", fahr);
+		}
+	}
+	if(c == '3')
+	{
+		printf ("Please enter Fahrenheit to be converted\n");
+		while (scanf("%f", &fahr) != EOF)
+		{
+			kelvin = (fahr + 459.67) * 5.0 / 9.0;
+			printf("Kelvin: %f\n", kelvin);
 		}
 	}
 }
