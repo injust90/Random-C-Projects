8e52f6692964cdeb0fcf79b0df1d42dc1966e27a
Justin Chou <justinjchou@gmail.com>
2025-11-15 08:17:26 -0800

    modified dialog and added notes

---
+++ simpledialog/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int randomChar();
+
+int main()
+{
+	int choice;
+
+	printf("Welcome the Library.\nA place of lost information\n");
+	printf("図書館へようこそ。\n");
+
+	choice = getchar();
+
+	// if (choice == '1') {
+	// 	printf("You");
+	// }
+	// if (choice == '2') {
+	// 	printf("");
+	// }
+
+	char c[] = "ア";
+	printf("%s", c);
+}
+
+int randomChar()
+{
+	return 0;	
+}
