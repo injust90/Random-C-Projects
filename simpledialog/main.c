5bd7431163d636687694990a0676bb7d3356db3e
Justin Chou <justinjchou@gmail.com>
2025-11-21 16:42:32 -0800

    Updated for one letter

--- simpledialog/main.c
+++ simpledialog/main.c
@@ -11,6 +11,8 @@
 
 int randomChar();
 int my_getline(wchar_t line[], int maxline);
+int hiraganaA();
+void introDialog();
 
 int main()
 {
@@ -22,37 +24,38 @@ int main()
 	int len;
 	wchar_t line[MAXLINE];
 
-	printf("You find yourself in a dusty old library.\n\nThe books are entombed with dust and there is a musky odor in the air.\n\n");
-	printf("A kind voice begins to speak \"I've been waiting a longgg time for you!\"\n\n");
-	printf("\"We need to learn an ancient language known as Japanese\"\n\n");
-	printf("\"This is going to be fun!\n\n\"");
-	printf("First thing is first, we need to teach you the basics.\n\n");
-	printf("There are 46 letters of hiragana (ひらがな) and 46 katakana (カタカナ)\n\n");
-	printf("The first letters of the alphabet are あいうえお corresponding to AIUEO.\n\n");
-	printf("I'm going to show you the letters with the romaji and it's your job to type them out.\n\n");
-	printf("For example if I show you カ(ka) it's your job to type out ka, got it? :)\n\n");
+	introDialog();
+	setlocale(LC_ALL, "en_US.utf8");
+	const char* mbstr = "あ";
+	wchar_t test[1];
+	mbstowcs(test, mbstr, 1);
+	if (test[0] == hiraganaA())
+		printf("Test Correct! \n");
+	else
+		printf("Test Incorrect! \n");
+	printf("test[0]: %lc", test[0]);
 
-	do {
-		katakana = randomChar();
-		printf("Input the correct katakana: %lc\n", katakana);
+	/*
+	// Main program
+	len = my_getline(line, MAXLINE);
+	if (len == 1) {
 
-		len = my_getline(line, MAXLINE);
-		if (len == 1) {
-			if (line[0] == katakana)
-				printf("Correct! \n");
-			else 
-				printf("Incorrect \n");
-		}
+
+		printf("line[0] %lc\n", line[0]);
+		if (test[0] == hiraganaA())
+			printf("Correct! \n");
 		else 
-			printf("Incorrect! Too many characters.\n");
+			printf("Incorrect \n");
 	}
-	while (len > 0);
+	else
+		printf("Incorrect! Too many characters.");
+	*/
 
    return 0;
 }
 
 // read a line into s, return length
-int my_getline(wchar_t s[], int lim) 
+int my_getline(wchar_t s[], int lim)
 {
 	wchar_t c;
 	int i;
@@ -62,6 +65,15 @@ int my_getline(wchar_t s[], int lim)
 	}
 	s[i] = '\0';
 	return i;
+}
+
+int hiraganaA()
+{
+	const char* mbstr = "あ";
+	wchar_t wstr[1];
+	mbstowcs(wstr, mbstr, 1);
+
+	return wstr[0];
 }
 
 int randomChar()
@@ -76,5 +88,18 @@ int randomChar()
 	wchar_t wstr[46];
 	mbstowcs(wstr, mbstr, 46);
 
-	return wstr[rd_num];	
+	return wstr[rd_num];
+}
+
+void introDialog()
+{
+
+	printf("You find yourself in an old library.\n\nThe books are entombed with dust and there is a musky odor in the air.\n\n");
+	printf("A kind voice begins to speak \"I've been waiting a longgg time for you!\"\n\n");
+	printf("\"We need to learn an ancient language known as Japanese\"\n\n");
+	printf("\"This is going to be fun!\n\n\"");
+	printf("First thing is first, we need to teach you the basics.\n\n");
+	printf("There are 46 letters of hiragana (ひらがな) and 46 katakana (カタカナ)\n\n");
+	printf("The first letters of the alphabet are あいうえお corresponding to AIUEO.\n\n");
+	printf("Each character represents a vowel, for example あ represents the sound 'a'\n\n");
 }
